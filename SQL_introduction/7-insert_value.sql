2e7c7517bff7583d80f29ca27529d2eff270e7f7
tsion mesfin <sionmesfin@gmail.com>
2024-02-06 16:40:58 +0300

    corection

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,1 +1,6 @@
+-- This script inserts a new row into the table first_table in the hbtn_0c_0 database.
+-- The new row will have id = 89 and name = 'Holberton School'.
+-- The database name will be passed as an argument of the mysql command.
+
+-- Insert statement to add a new row into the table
 INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
