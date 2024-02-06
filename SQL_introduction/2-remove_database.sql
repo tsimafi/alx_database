901c00317ca317a2d870ed191f7425c4e83e44b1
tsion mesfin <sionmesfin@gmail.com>
2024-02-06 16:27:57 +0300

    corection

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,2 +1,4 @@
--- List all tables in the specified database
-SHOW TABLES;
+-- 2-remove_database.sql
+
+-- Attempt to drop the database, if it exists
+DROP DATABASE IF EXISTS hbtn_0c_0;
--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,1 +1,7 @@
+-- 8-count_89.sql
+
+-- Use the passed database name
+USE hbtn_0c_0;
+
+-- Query to count records with id = 89 in the first_table
 SELECT COUNT(*) FROM first_table WHERE id = 89;
