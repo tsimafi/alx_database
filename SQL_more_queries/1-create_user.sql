37d44d6386fc0eff5ff220ca2f8e501df3c24f77
tsion mesfin <sionmesfin@gmail.com>
2024-02-17 15:50:23 +0300

    new file

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,11 +1,10 @@
-# Create a MySQL user named 'user_0d_1' with access from 'localhost'
-# If the user already exists, the script will not fail
-echo "CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';" | mysql -hlocalhost -uroot -p
+-- 1-create_user.sql
 
-# Grant all privileges on all databases and tables to 'user_0d_1' when accessing from 'localhost'
-echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
+-- Check if the user exists
+SELECT user FROM mysql.user WHERE user = 'user_0d_1' LIMIT 1;
 
-# Display the contents of the SQL file named 0-privileges.sql and pipe it to MySQL for execution
-# It seems the SQL file is expected to contain SQL queries related to privileges
-# The output of the commands will be displayed on the terminal
-cat 0-privileges.sql | mysql -hlocalhost -uroot -p
+-- If the user doesn't exist, create it
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
