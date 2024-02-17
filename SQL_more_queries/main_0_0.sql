e075f72c35a8d5d23e5553e3d5021a6794a3a7f0
tsion mesfin <sionmesfin@gmail.com>
2024-02-17 15:43:27 +0300

    new file

--- SQL_more_queries/main_0_0.sql
+++ SQL_more_queries/main_0_0.sql
@@ -1,6 +1,7 @@
 -- Attempt to fetch privileges for user_0d_1 and user_0d_2
 SELECT * FROM mysql.user WHERE user IN ('user_0d_1', 'user_0d_2') AND host = 'localhost';
 There is no such grant defined for user 'user_0d_1' on host 'localhost'
+
 -- Attempt to create user_0d_1
 CREATE USER 'user_0d_1'@'localhost';
 
