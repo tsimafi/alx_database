c4fea0f40f354e07949c8fe6c6e69eefdc1f8376
tsion mesfin <sionmesfin@gmail.com>
2024-02-17 16:04:42 +0300

    new file

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,16 @@
---USE hbtn_0d_2;
+-- This script lists all cities contained in the hbtn_0d_usa database along with their corresponding states.
+-- It retrieves the city ID, city name, and state name.
+-- Results are sorted in ascending order by city ID.
 
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL
-);
+SELECT 
+    cities.id,    -- Select the ID of the city
+    cities.name,  -- Select the name of the city
+    states.name   -- Select the name of the state
+FROM 
+    cities         -- Select from the cities table
+JOIN 
+    states         -- Perform a join with the states table
+ON 
+    cities.state_id = states.id   -- Match the state ID from the cities table with the ID from the states table
+ORDER BY 
+    cities.id ASC;  -- Sort the results in ascending order by city ID
