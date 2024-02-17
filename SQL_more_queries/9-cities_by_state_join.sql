466322b92f78ee0f0a61cdb4f540158a0cf2af4d
tsion mesfin <sionmesfin@gmail.com>
2024-02-17 16:52:59 +0300

    new file

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
-/* Script to list all cities contained in the database hbtn_0d_usa.
+/*-- Script to list all cities contained in the database hbtn_0d_usa.
 Each record should display: cities.id - cities.name - states.name
 Results must be sorted in ascending order by cities.id
 You can use only one SELECT statement
