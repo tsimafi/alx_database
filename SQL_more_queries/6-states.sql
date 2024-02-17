663d1d38ce33bf939475c45533625bed8a8ae8ff
tsion mesfin <sionmesfin@gmail.com>
2024-02-17 16:19:46 +0300

    new file

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,9 +1,14 @@
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the hbtn_0d_usa database
 USE hbtn_0d_usa;
 
+-- Create the states table if it doesn't exist
 CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
 
-INSERT INTO states (name) VALUES ('California'), ('Arizona'), ('Texas');
+-- Insert data into the states table
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas");
