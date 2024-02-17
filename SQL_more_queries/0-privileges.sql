b0b88c66f0222b03e578b12c1304c798f93a8a47
tsion mesfin <sionmesfin@gmail.com>
2024-02-17 15:31:19 +0300

    new file

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,22 +1,7 @@
-# Display the contents of the SQL file named 0-privileges.sql and pipe it to MySQL for execution
-# It seems the SQL file is expected to contain SQL queries related to privileges
-# The output of the commands will be displayed on the terminal
-cat 0-privileges.sql | mysql -hlocalhost -uroot -p
+-- Grant privileges to user_0d_1
+CREATE USER 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
-# Prompted for the password, it seems the MySQL server requires a password for root user access
-
-# Create a new MySQL user named 'user_0d_1' with access from 'localhost'
-echo "CREATE USER 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
-
-# Grant all privileges on all databases and tables to 'user_0d_1' when accessing from 'localhost'
-echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
-
-# Display the contents of the SQL file named 0-privileges.sql and pipe it to MySQL for execution
-# It seems the SQL file is expected to contain SQL queries related to privileges
-# The output of the commands will be displayed on the terminal
-cat 0-privileges.sql | mysql -hlocalhost -uroot -p
-
-# Prompted for the password again
-
-# The grants for user_0d_1@localhost are displayed
-# However, it seems there's an error related to user_0d_2@localhost
+-- Grant privileges to user_0d_2
+CREATE USER 'user_0d_2'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
