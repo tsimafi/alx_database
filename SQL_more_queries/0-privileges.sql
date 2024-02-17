56a3ee43837660fdfc5db33d44a26edf241ecbd4
tsion mesfin <sionmesfin@gmail.com>
2024-02-17 15:33:45 +0300

    new file

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -5,3 +5,7 @@ GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 -- Grant privileges to user_0d_2
 CREATE USER 'user_0d_2'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+
+-- Attempt to fetch privileges for user_0d_1 and user_0d_2
+SELECT * FROM mysql.user WHERE user IN ('user_0d_1', 'user_0d_2') AND host = 'localhost';
+There is no such grant defined for user 'user_0d_1' on host 'localhost'
