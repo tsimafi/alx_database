7befc47ad2f34a72dcf11672d0d10f0cf04bd4a1
tsion mesfin <sionmesfin@gmail.com>
2024-02-17 16:08:44 +0300

    new file

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,10 @@
+-- Use the specified database
+USE hbtn_0d_2;
+
+-- Create the table if it doesn't already exist
 CREATE TABLE IF NOT EXISTS id_not_null (
+    -- Define the id column with INT type and default value 1
     id INT DEFAULT 1,
+    -- Define the name column with VARCHAR(256) type
     name VARCHAR(256)
 );
